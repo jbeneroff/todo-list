0606a598fd4ef45751942a832497ddcd2bf59ce9
Jared Beneroff <jaredbeneroff@Jareds-MacBook-Pro.local>
2021-07-28 14:03:46 -0400

    signup

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
+import { signUp } from "../controllers/users.js"
 
 const router = Router()
 
 // sign up : POST
-router.post('/sign-up', )
+router.post('/sign-up', signUp)
 // sign in : POST
 router.post('/sign-in', )
 // verify: GET
